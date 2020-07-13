f059d5d034d3ea4dfdf31a1bebf719a6a65d8b32
Piyush <piyushgadge800@gmail.com>
2020-07-13 19:29:23 +0530

    second commit

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, PermissionsMixin
 
-# Create your models here.
+
+# Database Models for users in system
+class UserProfile(AbstractUser, PermissionsMixin):
+    email = models.EmailField(max_length=100, unique=True)
+    name = models.CharField(max_length=200)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    
+
