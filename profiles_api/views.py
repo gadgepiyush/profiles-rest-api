5c0b84a27fe2d8c770c1e48872f19b7eb035e7ed
Piyush <piyushgadge800@gmail.com>
2020-07-14 19:34:38 +0530

    added other methods

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,10 +1,13 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+from profiles_api import serializers
 
 
 # Create your views here.
 class HelloApiView(APIView):
     # Test api view
+    serializer_class = serializers.HelloSerializer
 
     def get(self, request, format=None):
         # Returns a list of APIView features
@@ -15,3 +18,27 @@ class HelloApiView(APIView):
             'is mapped manually to urls'
         ]
         return Response({'message': "hello", 'an_apiview': an_apiview})
+
+    def post(self, request):
+        # Create a hello message with our name
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello my name is {name}'
+            return Response({'message': message})
+
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, pk=None):
+        # Handle updating objects
+        return Response({'method': 'PUT'})
+
+    def patch(self, request, pk=None):
+        # Handle a partial update of an objects
+        return Response({'method': 'PATCH'})
+
+    def delete(self, request, pk=None):
+        # Deleting an objects
+        return Response({'method': 'DELETE'})
